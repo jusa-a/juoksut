c9640b6b3b620b678bafde37868b46a48b08d7c7
jusa-a <jusa.annevirta@aalto.fi>
2023-12-16 18:25:16 +0200

    configure tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+    content: [],
+    theme: {
+        extend: {
+            fontFamily: {
+                sans: ['Helvetica', 'sans-serif'],
+                serif: ['Georgia', 'serif']
+            },
+            colors: {
+                pink: '#FF639A'
+            }
+        }
+    },
+    plugins: []
+}
