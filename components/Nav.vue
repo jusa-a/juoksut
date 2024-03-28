090ee545e89b731fef2c3634f93da91e388e1797
jusa-a <jusa.annevirta@aalto.fi>
2024-03-28 15:17:27 +0200

    update nav

--- components/Nav.vue
+++ components/Nav.vue
@@ -1,12 +1,20 @@
 <template>
-    <nav
-        class="flex justify-between sticky top-0 z-99 p-1 h-[var(--nav-height)] divider-b">
-        <img src="/logo.svg" alt="JUOKSUT RUN CLUB" />
+    <nav class="flex sticky top-0 z-99 p-1 h-[var(--nav-height)] divider-b">
+        <img src="/logo.svg" alt="JUOKSUT RUN CLUB LEGS LOGO" />
 
-        <div class="flex flex-col justify-between text-right">
-            <NuxtLink to="/">HOME</NuxtLink>
-            <NuxtLink to="/join">JOIN</NuxtLink>
-            <NuxtLink to="/archive">ARCHIVE</NuxtLink>
+        <div class="navContainer flex flex-wrap justify-between items-end grow">
+            <NuxtLink class="home self-center" to="/">
+                <h1 id="juoksut" class="pl-[0.15em] pb-[0.07em] mt-[0.2em]">
+                    JUOKSUT
+                </h1>
+            </NuxtLink>
+
+            <div
+                class="navLinks flex grow justify-between gap-[2em] justify-self-end text-right pl-[0vw] pr-[0vw]">
+                <NuxtLink to="/join">JOIN</NuxtLink>
+                <NuxtLink to="/archive">ARCHIVE</NuxtLink>
+                <NuxtLink to="/shop">SHOP</NuxtLink>
+            </div>
         </div>
     </nav>
 </template>
@@ -19,7 +27,37 @@
         /* filter: invert(100%); */
     }
 
-    .router-link-active {
+    .home:hover {
+        transform: none;
+        opacity: 70%;
+    }
+
+    #juoksut {
+        @apply font-serif font-medium;
+        font-size: 4em;
+        line-height: 0.7em;
+
+        /* transition: 0.15s; */
+        transform: skewX(-8deg);
+    }
+
+    .navLinks {
+        max-width: 16.5em;
+    }
+
+    .navLinks a {
+        line-height: 1em;
+    }
+
+    .navLinks a::before {
+        display: block;
+        content: attr(title);
+        height: 0;
+        overflow: hidden;
+        visibility: hidden;
+    }
+
+    .navLinks > .router-link-active {
         transform: skewX(-10deg);
         text-decoration: underline;
     }
