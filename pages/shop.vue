31d99423283b587b4eeb78ff22d6cae8054441ef
jusa-a <jusa.annevirta@aalto.fi>
2024-03-28 23:11:10 +0200

    shop layout draft

---
+++ pages/shop.vue
@@ -0,0 +1,53 @@
+<template>
+    <section class="itemsContainer flex flex-col m-auto">
+        <NuxtLink to="" class="shopItem mb-[2em]">
+            <div class="thumb"><img src="" alt="" /></div>
+            <div class="text-center">
+                <p class="font-medium">Lightspeed Lifestyle Long Sleeve</p>
+                <p class="font-light">35€</p>
+            </div>
+        </NuxtLink>
+
+        <NuxtLink to="" class="shopItem mb-[2em]">
+            <div class="thumb"><img src="" alt="" /></div>
+            <div class="text-center">
+                <p class="font-medium">Lightspeed Lifestyle Long Sleeve</p>
+                <p class="font-light">35€</p>
+            </div>
+        </NuxtLink>
+    </section>
+
+    <section class="divider-t">
+        <video
+            ref="video"
+            class="object-cover object-center w-[100%] h-[12em]"
+            autoplay
+            playsinline
+            loop
+            muted>
+            <source src="/juoksut.mp4" type="video/mp4" />
+        </video>
+    </section>
+</template>
+
+<script setup></script>
+
+<style scoped>
+    .itemsContainer {
+        width: clamp(65%, 500px, 100%);
+    }
+
+    .shopItem:hover {
+        transform: none;
+        opacity: 100%;
+    }
+
+    .shopItem:hover p {
+        opacity: 70%;
+    }
+
+    .shopItem .thumb {
+        height: 22em;
+        background-color: pink;
+    }
+</style>
