1c629094495e803d4c05e3a651aa84909a1e476c
jusa-a <jusa.annevirta@aalto.fi>
2025-03-24 21:26:01 +0200

    add order info page

---
+++ pages/orders.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <section>
+      <h2>ORDER INFO & POLICY</h2>
+      <div>
+      </div>
+    </section>
+
+    <FooterVideo />
+  </div>
+</template>
+
+<script setup></script>
+
+<style scoped>
+p {
+  @apply mt-0;
+}
+</style>
